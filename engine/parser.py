db8d3a17a6223c7fe94238793522b3290cb14860
BandiKarthik362k <karthik362k@gmail.com>
2025-11-14 12:25:19 -0800

    parser

---
+++ engine/parser.py
@@ -0,0 +1,47 @@
+
+class csvreader:
+
+    def quote_split(self,s):
+        s = s.rstrip("\r\n")
+        out, buf, q = [], [], False
+        i, n = 0, len(s)
+
+        while i < n:
+            ch = s[i]
+            if ch == '"':
+                
+                if q and i + 1 < n and s[i + 1] == '"':
+                    buf.append('"'); i += 2
+                else:
+                    q = not q; i += 1
+            elif ch == ',' and not q:
+                out.append(''.join(buf)); buf = []; i += 1
+            else:
+                buf.append(ch); i += 1
+
+        out.append(''.join(buf))
+        return out
+
+
+    def read_doc(self,path):
+
+        b = []
+        try:
+            with open(path,'r',encoding="utf-8") as a:
+
+                for i in a:
+                    if i=="":
+                        continue 
+                    
+                    if '"' in i:
+                        b.append(self.quote_split(i))
+                    else:
+                        cur = i.strip().split(',')
+                        b.append(cur)
+                return b[0], b[1:]
+        except FileNotFoundError:
+            print("There is no file at the given path, please check")
+
+        
+        
+    
